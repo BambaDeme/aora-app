dfdc1dbfd914f04087dbae4184a6e8b9512fbb34
bambademe <cheikhbambademe@gmail.com>
2024-05-09 13:16:11 +0000

    bottoms tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+
+import { Tabs, Redirect } from 'expo-router'
+import {icons} from "../../constants";
+
+const TabIcon = ({icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image 
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+        {name}
+      </Text>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs screenOptions={{tabBarShowLabel: false}}>
+        <Tabs.Screen 
+          name="home" 
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({color, focused}) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            )
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
